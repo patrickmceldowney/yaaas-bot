0271221ca8c8746d0665b9fc3e37c0d27719f7f0
Patrick <patrickmceldowney@outlook.com>
2021-07-15 08:50:00 -0500

    adding yaaas gif

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,10 @@
-<script>
-	import wretch from 'wretch';
-	import {onMount} from 'svelte';
-
-	onMount(() => {
-		console.log(mounted);
-	});
-</script>
-
-<div class="image-container" style="background-image: url(images/broad-city-logo-5.png);">
-		<h4 style="color: #d1d1d1;">Yaaaaas</h4>
+<div class="image-container">
+	<img src="images/broad-city.gif" alt="yaaaas">
 </div>
 
 
 <style>
 	.image-container {
-		background-size: cover;
 		display: flex;
 		align-content: center;
 		justify-content: center;
