8d0b03ce3912fd220583a3088ef43fe41b58ed35
Patrick <patrickmceldowney@outlook.com>
2021-07-15 00:15:57 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import wretch from 'wretch';
+	import {onMount} from 'svelte';
+
+	onMount(() => {
+		console.log(mounted);
+	});
+</script>
+
+<div class="image-container" style="background-image: url(images/broad-city-logo-5.png);">
+		<h4 style="color: #d1d1d1;">Yaaaaas</h4>
+</div>
+
+
+<style>
+	.image-container {
+		background-size: cover;
+		display: flex;
+		align-content: center;
+		justify-content: center;
+	}
+</style>
